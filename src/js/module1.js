6f833385a6b2a4e25e7c691ad79c82a29a1c08b3
Katy Moe <katy@katy.moe>
2015-10-11 00:41:39 +0100

    test hot testing

--- src/js/module1.js
+++ src/js/module1.js
@@ -1,1 +1,1 @@
-export default 'module1 string 2';
+export default 'module1 string 3';
--- tests/module1.js
+++ tests/module1.js
@@ -1,7 +1,10 @@
 import module1 from '../src/js/module1';
 
 it('should work', function() {
-  console.log(module1);
   let x = 2;
   expect(1 < 2).toBe(true);
+});
+
+it('should hot reload', function() {
+  expect(module1).toBe('module1 string 3');
 });
